22d206fa3df970d70f557c8bd815db78786195e3
Vaithyanathan Ramanathan <vaithy31@gmail.com>
2020-06-06 17:25:47 +0530

    Initial commit

--- bashrc.sh
+++ bashrc.sh
@@ -1,3 +1,41 @@
 PS1="\[\e]0;UbuntuVM \a\]\[\e[1;37m\][\[\e[m\]\[\e[1;36m\]\u\[\e[m\]\[\e[1;37m\]@\[\e[m\]\[\e[1;32m\]\h\[\e[m\]\[\e[1;37m\]:\[\e[m\]\[\e[1;33m\]\W\[\e[m\]\[\e[1;37m\]]\[\e[m\]\[\e[1;37m\]\\$\[\e[m\] "
 
+\u - username
+\s - shell name
+\w - full pwd
+\W - last pwd
+\H - hostname
+\h - first hostname
+\! - history number
+\$ - root or non-root
+\d - date
+\D{} - formatted date
+\A - 24 time without seconds
+\t - 24 time with seconds
+\@ - 12 time without seconds
+\T - 12 time with seconds
+\\
+
 alias cls='clear'
+
+HISTSIZE
+HISTFILESIZE
+HISTTIMEFORMAT strftime format
+HISTCONTROL
+HISTIGNORE
+
+GREP_OPTIONS
+
+#Research or Document the understanding
+login shell
+
+bash_rc - resource
+bash_login
+bash_profile
+profile
+login
+
+1. /etc/profile
+
+sub-shell
+.bashrc
